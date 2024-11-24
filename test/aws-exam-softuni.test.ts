527eef3f3d41a5a69125d7827fd93e8017bf7172
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-11-24 13:02:33 +0100

    added basic snapshot test

--- test/aws-exam-softuni.test.ts
+++ test/aws-exam-softuni.test.ts
@@ -1,17 +1,9 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsExamSoftuni from '../lib/aws-exam-softuni-stack';
+import * as cdk from "aws-cdk-lib";
+import * as AwsExamSoftuni from "../lib/aws-exam-softuni-stack";
+import "jest-cdk-snapshot";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-exam-softuni-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsExamSoftuni.AwsExamSoftuniStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("Lambda Function Created", () => {
+  const app = new cdk.App();
+  const stack = new AwsExamSoftuni.AwsExamSoftuniStack(app, "MyTestStack");
+  expect(stack).toMatchCdkSnapshot();
 });
