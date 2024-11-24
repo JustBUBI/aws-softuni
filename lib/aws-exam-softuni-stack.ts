e79e22a6c09500492c638bca9368b4f3ec5891fa
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-11-24 11:39:52 +0100

    Added  SNS topic and lambda integration

--- lib/aws-exam-softuni-stack.ts
+++ lib/aws-exam-softuni-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from "aws-cdk-lib";
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { CfnRestApi, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
 
 export class AwsExamSoftuniStack extends cdk.Stack {
@@ -26,13 +28,27 @@ export class AwsExamSoftuniStack extends cdk.Stack {
       handler: "handler",
     });
 
+    // SNS topic
+    const fileUploadTopic = new Topic(this, "FileUploadTopic", {
+      displayName: "FileUploadTopic",
+    });
+    // TODO: Change email to: hristo.zhelev@yahoo.com.
+    fileUploadTopic.addSubscription(new EmailSubscription("lyubomir555@gmail.com"));
+
     // Api Gateway
-    const api = new RestApi(this, "ProcessorApi");
+    const api = new RestApi(this, "FileUploadApi");
     const resource = api.root.addResource("fileUpload");
-    resource.addMethod("POST", new LambdaIntegration(fileUploadFunction));
+    resource.addMethod(
+      "POST",
+      new LambdaIntegration(fileUploadFunction, { proxy: true })
+    );
+
+    // Enable Binary Media Types
+    const cfnApi = api.node.defaultChild as CfnRestApi;
+    cfnApi.addPropertyOverride("BinaryMediaTypes", ["multipart/form-data"]);
 
     // Outputs
-    new cdk.CfnOutput(this, 'DynamoDBTableName', {
+    new cdk.CfnOutput(this, "DynamoDBTableName", {
       value: fileMetadataTable.tableName,
     });
     new cdk.CfnOutput(this, "RestApiEndpoint", {
