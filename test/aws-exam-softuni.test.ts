fa81333dbcfd4a44dd3f38aaff16ee29f1bdf88b
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-11-24 08:49:55 +0100

    cdk init

---
+++ test/aws-exam-softuni.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsExamSoftuni from '../lib/aws-exam-softuni-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-exam-softuni-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsExamSoftuni.AwsExamSoftuniStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
