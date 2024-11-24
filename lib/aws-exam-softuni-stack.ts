0f63a19b7f0544db4c34e35e4eb03cca1735b7bc
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-11-24 09:12:52 +0100

    Added file metadata daynamodb table

--- lib/aws-exam-softuni-stack.ts
+++ lib/aws-exam-softuni-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 
 export class AwsExamSoftuniStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fileMetadataTable = new Table(this, "FileMetadataTable", {
+      tableName: "FileMetadataTable",
+      partitionKey: {
+        name: "fileExtension",
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsExamSoftuniQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output for debugging
+    new cdk.CfnOutput(this, 'DynamoDBTableName', {
+      value: fileMetadataTable.tableName,
+    });
   }
 }
